c590dc340db987503c4966f2464cc22aa3417d83
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2020-09-28 20:00:04 +0100

    Added Pictures API functionality to main.js and amended index.html & style.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,18 @@ $(document).ready(function () {
 
         var query_country = $("#country-list").val();
 
-        //Empty 
-        $(".details").empty();
-        $("#selected-country").empty();
-        $("#national-tourism-articles").empty();
+        //Display Header Elements
+        $(".hide").removeClass("hide");
 
-        //Country Details 
+        //Empty
+
+        $(".details").empty();
+        $(".selected-country").empty();
+        $("#national-tourism-articles").empty();
+        $("#photo").empty();
+
+        //Country Details from Rest Countries API
+
         var country_endpoint = "https://restcountries.eu/rest/v2/name/";
         var country_remainder = "?fullText=true";
 
@@ -22,11 +28,12 @@ $(document).ready(function () {
                 <h4>Population:<span class="country-info">${data[i].population}</span></h4>
                 <h4>Currency:<span class="country-info">${data[i].currencies[0].name}</span></h4>
                 <h4>Borders:<span class="country-info">${data[i].borders}</span></h4>`);
-                $("#selected-country").append(`${data[i].name}`);
+                $(".selected-country").append(`${data[i].name}`);
             }
         });
 
-        //Tourism Articles
+        //Tourism Articles from New York Times Articles API
+
         var article_endpoint = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=tourism-";
         var article_midURL = "&fq=news_desk:(travel)ANDgeoocations:";
         var article_apiKey = "&api-key=mTYJd3lj52S8QoXeLzYlZiGGswIRLX7N";
@@ -41,14 +48,18 @@ $(document).ready(function () {
                 <br><br>`);
             }
         });
+
+        //Pictures from Pixabay API
+
+        var picture_endpoint = "https://pixabay.com/api/?key=";
+        var picture_apiKey = "18432792-6a5edf4c31f5b9b4e47dd6528";
+        var picture_query = "&q=";
+
+        $.getJSON(`${picture_endpoint}${picture_apiKey}${picture_query}${query_country}`, function (data) {
+            var photos = data.hits;
+            for (var i = 0; i < 9; i++) {
+                $("#photo").append(`<div class="api-photo"><img src="${photos[i].largeImageURL}" height="300px" width="300px"></div>`);
+            }
+        });
     });
 })
-
-
-
-
-// Picture Section
-
-// $.getJSON("https://pixabay.com/api/?key=18432792-6a5edf4c31f5b9b4e47dd6528&q=ireland", function (data) {
-//     console.log(data.hits[0].largeImageURL);
-// })
